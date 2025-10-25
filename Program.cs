ca13cdaa83aa3e2ca65ecaf770fe39a0d1d8af3a
Тимофей <@DESKTOP-UIQUUA6>
2025-10-25 15:55:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine();
+{
+    double num1 = 0;
+    double num2 = 0;
+    string operation;
+
+    Console.WriteLine(" Консольный калькулятор ");
+    Console.Write(" Введите первое число: ");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write(" Введите нужный знак исчисления (+, -, *, /): ");
+    operation = Console.ReadLine();
+    Console.Write(" Введите второе число: ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+    double result = 0;
+    switch (operation)
+    {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: деление на ноль невозможно.");
+                return;
+            }
+            break;
+        default:
+            Console.WriteLine("Ошибка: неверная операция.");
+            return;
+    }
+    Console.WriteLine($"Результат: {result}");
+    Console.ReadKey();
+}
